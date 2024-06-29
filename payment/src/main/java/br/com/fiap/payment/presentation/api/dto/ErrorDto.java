3272c2939fdfd44a72b86676468b295853b2a1f0
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-06-29 11:15:18 -0300

    config(payment): added API configurations.

---
+++ payment/src/main/java/br/com/fiap/payment/presentation/api/dto/ErrorDto.java
@@ -0,0 +1,12 @@
+package br.com.fiap.payment.presentation.api.dto;
+
+import org.springframework.validation.FieldError;
+
+public record ErrorDto(
+    String field,
+    String message) {
+
+  public ErrorDto(FieldError fieldError) {
+    this(fieldError.getField(), fieldError.getDefaultMessage());
+  }
+}
